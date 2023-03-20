0eb752927be4590800cdb8aa93f7899ebd8769ca
Carlos Pío Reyes <alu0101132945@ull.edu.es>
2023-03-20 11:02:53 +0000

    unido los dos ficheros specs

--- tests/sort.spec.ts
+++ tests/sort.spec.ts
@@ -19,4 +19,12 @@ describe('Sort methods tests', () => {
             expect(mySort.getSortStrategy()).to.deep.equal(merg);
         });
     })
+    context('Bubblesort methods tests', () =>{
+        const mySort = new Sort([6,2,8,5,1,3],new Mergesort);
+        const mySort2 = new Sort([10,2,120,46,31,660],new Mergesort);
+        it('logic tests', () => {
+            expect(mySort.logic()).to.deep.equal([1,2,3,5,6,8]);
+            expect(mySort2.logic()).to.deep.equal([2,10,31,46,120,660]);
+        });
+    })
 });
